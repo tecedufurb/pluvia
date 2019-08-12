577ef6ec1d9cec0c280bb38fae938d7770f6314f
AlexSerodio <alequis01@gmail.com>
2019-08-12 18:15:18 -0300

    Add gaze and new rain prefab

---
+++ Assets/Pluvia/Scripts/ClimateManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using DigitalRuby.RainMaker;
+using UnityEngine;
+
+public class ClimateManager : MonoBehaviour{
+    
+    [SerializeField] private RainScript rainMaker;
+    [SerializeField] private Transform river;
+    [SerializeField] private Transform waterFall;
+    private Vector3 waterFallInitialPosition;
+    private Vector3 waterFallFinalPosition;
+    private float minPosition = .7f;
+    private float maxPosition = 12.2f;
+    private float speed = 2f;
+
+    private void Start() {
+        waterFallInitialPosition = waterFall.position;
+        waterFallFinalPosition = new Vector3(waterFall.position.x, -20f, waterFall.position.z);
+    }
+    
+    /// <summary>
+    /// Starts the coroutine MoveRiverUp witch will activate the rain and the waterfall (if they were disable)
+    /// and move the river up while the player keeps pressing the button. 
+    /// Called in the EventTrigger of the RainButton button.
+    /// </summary>
+    public void StartRain() {
+        StartCoroutine(MoveRiverUp());
+    }
+    
+    /// <summary>
+    /// Starts the coroutine MoveRiverDown witch will disable the rain and move the river down
+    /// while the player keeps pressing the button. If the river reaches its limit the waterfall
+    /// will be disable too.
+    /// Called in the EventTrigger of the DryButton button.
+    /// </summary>
+    public void StartDry() {
+        StartCoroutine(MoveRiverDown());
+    }
+
+    /// <summary>
+    /// Stops the coroutine MoveRiverUp and disables the rain.
+    /// </summary>
+    public void StopRain() {
+        StopAllCoroutines();
+        DisableRain();
+        rainMaker.RainIntensity = 0;
+    }
+
+    private IEnumerator MoveRiverUp () {
+        EnableRain();
+        if (!waterFall.position.Equals(waterFallInitialPosition))
+            waterFall.position = waterFallInitialPosition;   
+                
+        yield return new WaitForSeconds(2f);
+        while (river.position.y < maxPosition) {
+            river.position = new Vector3(river.position.x, 
+                river.position.y + Time.deltaTime * speed, river.position.z);
+            yield return null;
+        }
+        DisableRain();
+    }
+
+    private IEnumerator MoveRiverDown() {
+        DisableRain();
+        yield return new WaitForSeconds(.5f);
+        while (river.position.y > minPosition) {
+            river.position = new Vector3(river.position.x, 
+                river.position.y - Time.deltaTime * speed, river.position.z);
+            yield return null;
+        }
+        waterFall.position = waterFallFinalPosition;
+    }
+
+    private void EnableRain() {
+        rainMaker.RainIntensity = 1;
+    }
+
+    private void DisableRain() {
+        rainMaker.RainIntensity = 0;
+    }
+
+}
---
+++ Assets/Pluvia/Scripts/VrController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VrController : MonoBehaviour {
+    
+    enum SDKType {
+        Cardboard, None
+    }
+
+    [SerializeField] private SDKType VrType;
+
+    void Start() {
+        StartCoroutine(ChangeVrSdk());
+    }
+
+    private IEnumerator ChangeVrSdk() {
+        XRSettings.LoadDeviceByName(VrType.ToString());
+        yield return null;
+        XRSettings.enabled = true;
+    }
+
+}
